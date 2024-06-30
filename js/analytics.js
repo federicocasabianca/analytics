221e3223424652b8d83e94cd549b288d3317d8fc
Federico <federico.casabianca@gmail.com>
2024-06-30 15:48:35 +0200

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Nothing to load");
+    window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1})).promise.then(function() {window.amplitude.add(window.amplitudeAutocapturePlugin.plugin());window.amplitude.init('1dc04bc19b9e709dbc690b1f2e1d1381');});
 });
