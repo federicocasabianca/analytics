cfc441c9b61eeb8c312fb05c588ab8abd8a8fc7d
Federico <federico.casabianca@gmail.com>
2024-07-01 08:36:11 +0200

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Nothing to load");
     window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1})).promise.then(function() {window.amplitude.add(window.amplitudeAutocapturePlugin.plugin());window.amplitude.init('1dc04bc19b9e709dbc690b1f2e1d1381');});
+
+      // Function to log button clicks
+    function logButtonClick(buttonId) {
+        console.log(`Button click logged: ${buttonId}`);
+    }
+
+        // Add event listeners for button clicks
+    const addToCartButton = document.getElementById('add-to-cart');
+    if (addToCartButton) {
+        addToCartButton.addEventListener('click', function() {
+            logButtonClick('add-to-cart');
+        });
+    }
 });
